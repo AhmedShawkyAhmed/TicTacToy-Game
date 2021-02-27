4732755323cf9db49d7525bc5663a16f2757dde2
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-02-27 02:05:37 +0200

    Update

--- 2Players.py
+++ 2Players.py
@@ -13,39 +13,39 @@ style = ttk.Style()
 style.theme_use('winnative')
 
 #Add Buttons
-Bu1=ttk.Button(window, text=' ')
+Bu1 = ttk.Button(window, text=' ')
 Bu1.grid(row=0, column=0, sticky='snew', ipadx=30, ipady=40)
 Bu1.config(command=lambda: ButtonClick(1))
 
-Bu2=ttk.Button(window, text=' ')
+Bu2 = ttk.Button(window, text=' ')
 Bu2.grid(row=0, column=1, sticky='snew', ipadx=30, ipady=40)
 Bu2.config(command=lambda: ButtonClick(2))
 
-Bu3=ttk.Button(window, text=' ')
+Bu3 = ttk.Button(window, text=' ')
 Bu3.grid(row=0, column=2, sticky='snew', ipadx=30, ipady=40)
 Bu3.config(command=lambda: ButtonClick(3))
 
-Bu4=ttk.Button(window, text=' ')
+Bu4 = ttk.Button(window, text=' ')
 Bu4.grid(row=1, column=0, sticky='snew', ipadx=30, ipady=40)
 Bu4.config(command=lambda: ButtonClick(4))
 
-Bu5=ttk.Button(window, text=' ')
+Bu5 = ttk.Button(window, text=' ')
 Bu5.grid(row=1, column=1, sticky='snew', ipadx=30, ipady=40)
 Bu5.config(command=lambda: ButtonClick(5))
 
-Bu6=ttk.Button(window, text=' ')
+Bu6 = ttk.Button(window, text=' ')
 Bu6.grid(row=1, column=2, sticky='snew', ipadx=30, ipady=40)
 Bu6.config(command=lambda: ButtonClick(6))
 
-Bu7=ttk.Button(window, text=' ')
+Bu7 = ttk.Button(window, text=' ')
 Bu7.grid(row=2, column=0, sticky='snew', ipadx=30, ipady=40)
 Bu7.config(command=lambda: ButtonClick(7))
 
-Bu8=ttk.Button(window, text=' ')
+Bu8 = ttk.Button(window, text=' ')
 Bu8.grid(row=2, column=1, sticky='snew', ipadx=30, ipady=40)
 Bu8.config(command=lambda: ButtonClick(8))
 
-Bu9=ttk.Button(window, text=' ')
+Bu9 = ttk.Button(window, text=' ')
 Bu9.grid(row=2, column=2, sticky='snew', ipadx=30, ipady=40)
 Bu9.config(command=lambda: ButtonClick(9))
 
@@ -69,31 +69,31 @@ def ButtonClick(id):
 
 def SetLayout(ID, PlayerSymbol):
 
-    if ID==1:
+    if ID == 1:
         Bu1.config(text=PlayerSymbol)
         Bu1.state(['disabled'])
-    elif ID==2:
+    elif ID == 2:
         Bu2.config(text=PlayerSymbol)
         Bu2.state(['disabled'])
-    elif ID==3:
+    elif ID == 3:
         Bu3.config(text=PlayerSymbol)
         Bu3.state(['disabled'])
-    elif ID==4:
+    elif ID == 4:
         Bu4.config(text=PlayerSymbol)
         Bu4.state(['disabled'])
-    elif ID==5:
+    elif ID == 5:
         Bu5.config(text=PlayerSymbol)
         Bu5.state(['disabled'])
-    elif ID==6:
+    elif ID == 6:
         Bu6.config(text=PlayerSymbol)
         Bu6.state(['disabled'])
-    elif ID==7:
+    elif ID == 7:
         Bu7.config(text=PlayerSymbol)
         Bu7.state(['disabled'])
-    elif ID==8:
+    elif ID == 8:
         Bu8.config(text=PlayerSymbol)
         Bu8.state(['disabled'])
-    elif ID==9:
+    elif ID == 9:
         Bu9.config(text=PlayerSymbol)
         Bu9.state(['disabled'])
 
